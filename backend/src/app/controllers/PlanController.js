8ac6f8b01575afb0f1e74818b7275a9907e190b4
Matheus Kindrazki <matheuskindrazki@gmail.com>
2019-12-19 19:02:27 -0300

    criando seed de planos default

---
+++ backend/src/app/controllers/PlanController.js
@@ -0,0 +1,15 @@
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(req, res) {}
+
+  async show(req, res) {}
+
+  async store(req, res) {}
+
+  async update(req, res) {}
+
+  async delete(req, res) {}
+}
+
+export default new PlanController();
