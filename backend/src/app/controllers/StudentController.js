4a8fab040b18436756ec17bcc03a3e96db47b017
Matheus Kindrazki <matheuskindrazki@gmail.com>
2019-12-19 18:37:24 -0300

    Update das informações do estudante

--- backend/src/app/controllers/StudentController.js
+++ backend/src/app/controllers/StudentController.js
@@ -4,7 +4,7 @@ import Student from '../models/Student';
 
 class StudentController {
   async index(req, res) {
-    const students = Student.findAll();
+    const students = await Student.findAll();
 
     return res.json(students);
   }
@@ -12,7 +12,7 @@ class StudentController {
   async show(req, res) {
     const { id } = req.params;
 
-    const student = Student.findByPk(id);
+    const student = await Student.findByPk(id);
 
     if (!student) {
       return res.status(400).json({ error: 'Student does not exist.' });
@@ -36,15 +36,59 @@ class StudentController {
       return res.status(400).json({ error: 'Validation failed.' });
     }
 
-    const userExists = await Student.findOne({
+    const studentExists = await Student.findOne({
       where: { email: req.body.email },
     });
 
-    if (userExists) {
+    if (studentExists) {
       return res.status(400).json({ error: 'Student already exists.' });
     }
 
     const { name, email, age, weight, height } = await Student.create(req.body);
+
+    return res.json({
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const student = await Student.findByPk(id, {
+      attributes: ['id', 'name', 'email', 'age', 'weight', 'height'],
+    });
+
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exist.' });
+    }
+
+    if (req.body.email) {
+      const studentExists = await Student.findOne({
+        where: { email: req.body.email },
+      });
+
+      if (studentExists && studentExists.id !== student.id) {
+        return res.status(400).json({ error: 'E-mail already registered' });
+      }
+    }
+
+    const { name, email, age, weight, height } = await student.update(req.body);
 
     return res.json({
       name,
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,5 +12,11 @@ routes.post('/sessions', SessionController.store);
 
 /** Autênticação das rotas */
 routes.use(authMiddleware);
+
+/* CRUD de Estudantes */
+routes.get('/students', StudentController.index);
+routes.get('/students/:id', StudentController.show);
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
